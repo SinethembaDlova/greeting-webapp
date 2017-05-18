4c7a38e0e3b0332f57c1ee14bc9fe001324248ab
SinethembaDlova <dSinethemba@gmail.com>
2017-05-18 17:40:55 +0200

    displaying the names greeted

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
+// call express and store in a variable
 var express = require('express');
 var app = express();
 
 //start the server
 var server = app.listen(3000);
 
+
+
 app.get('/', function (req, res) {
  res.send('Greeting webapp!');
 });
 
+var storedNames = [];
+
+//creating people
 app.get('/greeting/:name', function(req, res){
     res.send("Hello, " + req.params.name)
-});
+    storedNames.push(req.params.name);
 
+});
+ //console.log(storedNames);
+
+//displaying all the created names
 app.get('/greeted', function(req, res){
-    res.send(req.params.name)
+   var displayName = '';
+   for (var i = 0; i < storedNames.length; i++) {
+     displayName += storedNames[i] + "<br>";
+     //console.log(storedNames[i]);
+   }
+   res.send(displayName);
+
 });
 
-app.get('/counter/<USER_NAME>', function(req, res){
+//displaying how many times someone has been greeted
+/*app.get('/counter/<USER_NAME>', function(req, res){
     res.send(Hello, <USER_NAME> has been greeted <COUNTER> times.)
-});
+});*/
