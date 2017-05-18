c0ffdfd52aeca3464cff37eea98aa01941e8e094
SinethembaDlova <dSinethemba@gmail.com>
2017-05-18 11:56:10 +0200

    adding more routes

--- index.js
+++ index.js
@@ -11,3 +11,11 @@ app.get('/', function (req, res) {
 app.get('/greeting/:name', function(req, res){
     res.send("Hello, " + req.params.name)
 });
+
+app.get('/greeted', function(req, res){
+    res.send(req.params.name)
+});
+
+app.get('/counter/<USER_NAME>', function(req, res){
+    res.send(Hello, <USER_NAME> has been greeted <COUNTER> times.)
+});
