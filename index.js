8099b76d564df4820ce4edefb1ec74dda9d37df8
SinethembaDlova <dSinethemba@gmail.com>
2017-05-19 15:58:50 +0200

    counting for each person and not duplicating names

--- index.js
+++ index.js
@@ -4,44 +4,44 @@ var app = express();
 
 //start the server
 var server = app.listen(3000);
+var storedNames = [];
+//var nameGreeted = {};
 
-
-
-app.get('/', function (req, res) {
- res.send('Greeting webapp!');
+app.get('/', function(req, res) {
+    res.send('Greeting webapp!');
 });
 
-var storedNames = [];
-var nameGreeted = {};
-var counter = 0;
 
 //creating people
-app.get('/greeting/:name', function(req, res){
+app.get('/greeting/:name', function(req, res) {
     res.send("Hello, " + req.params.name)
     storedNames.push(req.params.name);
-
+    //console.log(storedNames);
 });
- //console.log(storedNames);
-
 //displaying all the created names
-app.get('/greeted', function(req, res){
-   var displayName = '';
-   for (var i = 0; i < storedNames.length; i++) {
-     displayName += storedNames[i] + "<br>";
-     //console.log(storedNames[i]);
-   }
-   res.send(displayName);
+app.get('/greeted', function(req, res) {
+    var uniqueNames = [];
+    var displayName = '';
+
+    for (var i = 0; i < storedNames.length; i++) {
+        if (uniqueNames.indexOf(storedNames[i]) == -1) {
+            displayName += storedNames[i] + "<br>";
+            uniqueNames.push(storedNames[i]);
+        }
+    }
+    console.log(uniqueNames);
+    res.send(displayName);
 
 });
 
 
 //displaying how many times someone has been greeted
-app.get('/counter/:name', function(req, res){
-  for (var i = 0; i < storedNames.length; i++) {
-    if (req.params.name === storedNames[i]){
-      //nameGreeted[req.params.name] = true;
-      counter++;
+app.get('/counter/:name', function(req, res) {
+    var counter = 0;
+    for (var i = 0; i < storedNames.length; i++) {
+        if (req.params.name === storedNames[i]) {
+            counter++;
+        }
     }
-  }
-     res.send("Hello, " + req.params.name + " has been greeted " + counter + " times.");
+    res.send("Hello, " + req.params.name + " has been greeted " + counter + " times.");
 });
