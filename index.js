8abe34255c27d44327ff27c8b40e756ce72aea67
SinethembaDlova <dSinethemba@gmail.com>
2017-05-21 16:12:11 +0200

    displaying

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ var bodyParser = require('body-parser');
 var app = express();
 
 app.use(express.static('public'));
-app.use(bodyParser);
 app.engine('hbs', exphbs({defaultLayout: "main", extname: 'hbs'}));
 app.set('view engine', 'hbs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 //start the server
@@ -20,46 +21,45 @@ app.get('/', function(req, res) {
 
 app.post('/', function(req, res){
   var inputName = req.body.takeName;
-  console.log(req.body);
-  res.render("html_forms", {name:inputName});
+  console.log(inputName);
+  res.render("html_forms");
+  res.send({name:inputName});
 });
 
 
-// var storedNames = [];
-// //creating people
-// app.get('/greeting/:name', function(req, res) {
-//   res.send("Hello, " + req.params.name);
-//   //res.render("html_forms_greeting");
-//   storedNames.push(req.params.name);
-// });
-//
-// //displaying all the created names
-// app.get('/greeted', function(req, res) {
-//     var uniqueNames = [];
-//     var displayName = '';
-//
-//     for (var i = 0; i < storedNames.length; i++) {
-//         if (uniqueNames.indexOf(storedNames[i]) == -1) {
-//             displayName += storedNames[i] + "<br>";
-//             uniqueNames.push(storedNames[i]);
-//         }
-//     }
-//     console.log(uniqueNames);
-//     res.send(displayName);
-//
-// });
-//
-//
-// //displaying how many times someone has been greeted
-// app.get('/counter/:name', function(req, res) {
-//     var counter = 0;
-//     for (var i = 0; i < storedNames.length; i++) {
-//         if (req.params.name === storedNames[i]) {
-//             counter++;
-//         }
-//     }
-//     res.send("Hello, " + req.params.name + " has been greeted " + counter + " times.");
-// });
+ var storedNames = [];
+ //creating people
+app.get('/greeting/:name', function(req, res) {
+   res.send("Hello, " + req.params.name);
+   storedNames.push(req.params.name);
+ });
+
+ //displaying all the created names
+ app.get('/greeted', function(req, res) {
+    var uniqueNames = [];
+    var displayName = '';
+
+     for (var i = 0; i < storedNames.length; i++) {
+       if (uniqueNames.indexOf(storedNames[i]) == -1) {
+            displayName += storedNames[i] + "<br>";
+            uniqueNames.push(storedNames[i]);
+         }
+     }
+     res.send(displayName);
+
+ });
+
+
+//displaying how many times someone has been greeted
+ app.get('/counter/:name', function(req, res) {
+     var counter = 0;
+     for (var i = 0; i < storedNames.length; i++) {
+         if (req.params.name === storedNames[i]) {
+             counter++;
+         }
+     }
+     res.send("Hello, " + req.params.name + " has been greeted " + counter + " times.");
+ });
 
 app.listen(3000, function(err) {
   if (err) {
