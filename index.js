22bd84db79d261b90205567e76f15aa7906105b3
SinethembaDlova <dSinethemba@gmail.com>
2017-05-22 16:05:35 +0200

    diplayed greeting

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 // Gain access to express, express-handlebars and body-parser
 var express = require('express');
-var exphbs  = require('express-handlebars');
+var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
 
 var app = express();
 
-app.engine('hbs', exphbs({defaultLayout: "main", extname: 'hbs'}));
+app.engine('hbs', exphbs({
+    defaultLayout: "main",
+    extname: 'hbs'
+}));
 app.set('view engine', 'hbs');
 app.use(express.static('public'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 
 //start the server
@@ -19,60 +24,63 @@ app.get('/', function(req, res) {
     //res.send('Greeting webapp!');
 });
 
-app.post('/', function(req, res){
-  var inputName = req.body.takeName;
+app.post('/', function(req, res) {
+    var inputName = req.body.takeName;
 
-  //console.log(inputName);
-  //res.render("html_forms");
-  //res.send({name:inputName});
+    //console.log(inputName);
+    //res.render("html_forms");
+    //res.send({name:inputName});
 
-  res.redirect('greeting/' + inputName);
+    res.redirect('greeting/' + inputName);
 });
 
 
- var storedNames = [];
- //creating people
+var storedNames = [];
+//creating people
 app.get('/greeting/:name', function(req, res) {
-   //res.send("Hello, " + req.params.name);
+    //res.send("Hello, " + req.params.name);
 
-   res.render('html_forms', {name: req.params.name});
-   storedNames.push(req.params.name);
+    res.render('html_forms_greeting', {
+        name: req.params.name
+    });
+    storedNames.push(req.params.name);
 
- });
+});
 
- //displaying all the created names
- app.get('/greeted', function(req, res) {
+//displaying all the created names
+app.get('/greeted', function(req, res) {
     var uniqueNames = [];
     var displayName = '';
 
-     for (var i = 0; i < storedNames.length; i++) {
-       if (uniqueNames.indexOf(storedNames[i]) == -1) {
+    for (var i = 0; i < storedNames.length; i++) {
+        if (uniqueNames.indexOf(storedNames[i]) == -1) {
             displayName += storedNames[i] + "<br>";
             uniqueNames.push(storedNames[i]);
-         }
-     }
-     res.send(displayName);
+        }
+    }
+    res.render('html_forms_greeted');
+    //res.send(displayName);
 
- });
+});
 
 
 //displaying how many times someone has been greeted
- app.get('/counter/:name', function(req, res) {
-     var counter = 0;
-     for (var i = 0; i < storedNames.length; i++) {
-         if (req.params.name === storedNames[i]) {
-             counter++;
-         }
-     }
-     res.send("Hello, " + req.params.name + " has been greeted " + counter + " times.");
- });
+app.get('/counter/:name', function(req, res) {
+    var counter = 0;
+    for (var i = 0; i < storedNames.length; i++) {
+        if (req.params.name === storedNames[i]) {
+            counter++;
+        }
+    }
+    res.send("Hello, " + req.params.name + " has been greeted " + counter + " times.");
+});
 
 app.listen(3000, function(err) {
-  if (err) {
-    return err;
-  } else {
-    console.log('server running');
-  }
+    if (err) {
+        return err;
+    } else {
+        console.log('server running');
+    }
 });
 
 /*var iqhosha = document.querySelector(".inputCover button");
