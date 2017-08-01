ed4f55cca59a4c9fe4e9ac4bb9302fc13e9a9d94
SinethembaDlova <dSinethemba@gmail.com>
2017-08-01 16:13:03 +0200

    installing mongo and mongoose

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 'use strict';
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/my_database');
 
-
-const mongoURL = process.env.MONGO_DB_URL || "'mongodb://localhost/test'";
-mongoose.connect(mongoURL);
+const mongoURL = process.env.MONGO_DB_URL || "
+mongodb://sinethembadlova:t@snur@940521.mlab.com:37040/greeted_names";
+mongoose.connect(mongoURL, function(err, db) {
+if (err) {
+    throw err;
+} else {
+    console.log("successfully connected to the database");
+}
+db.close();
+});
