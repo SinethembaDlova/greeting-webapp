3b22efb21ca3a97deb8099893bfd9f8c9d4a5f10
SinethembaDlova <dSinethemba@gmail.com>
2017-08-02 16:37:59 +0200

    trying to store data on database

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-'use strict';
 var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/greeted_names');
 
-const mongoURL = process.env.MONGO_DB_URL || "
-mongodb://sinethembadlova:t@snur@940521.mlab.com:37040/greeted_names";
-mongoose.connect(mongoURL, function(err, db) {
-if (err) {
-    throw err;
-} else {
-    console.log("successfully connected to the database");
-}
-db.close();
+var greetedNames = mongoose.model('greeted_names', {
+    name: String
+});
+
+var names = new greeted_names({
+    name: 'Sinethemba'
+});
+names.save(function(err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('hello');
+    }
 });
